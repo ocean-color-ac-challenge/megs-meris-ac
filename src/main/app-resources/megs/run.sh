86262a9f4dc0480096794576b105340905733d45
fbrito <fbrito@sb-10-16-10-20.dev.terradue.int>
2014-10-14 12:22:59 +0200

    update

--- src/main/app-resources/megs/run.sh
+++ src/main/app-resources/megs/run.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# Project:		 ${project.name}
-# Author:		  $Author: fbrito $ (Terradue Srl)
-# Last update:	${doc.timestamp}:
-# Element:		 ${project.name}
-# Context:		 ${project.artifactId}
-# Version:		 ${project.version} (${implementation.build})
-# Description:	${project.description}
-#
 # This document is the property of Terradue and contains information directly
 # resulting from knowledge and experience of Terradue.
 # Any changes to this code is forbidden without written consent from Terradue Srl
 #
 # Contact: info@terradue.com
-# 2012-02-10 - NEST in jobConfig upgraded to version 4B-1.1
 
 # source the ciop functions (e.g. ciop-log)
-
 source ${ciop_job_include}
 export LC_ALL="en_US.UTF-8"
 
@@ -32,6 +22,7 @@ function cleanExit ()
 	case "$retval" in
 		$SUCCESS) 	msg="Processing successfully concluded";;
 		$ERR_NOINPUT)	msg="Input not retrieved to local node";;
+		$ERR_MEGS)	msg="megs returned an error";;
 		*)		msg="Unknown error";;
 	esac
 
@@ -64,43 +55,44 @@ do
 	ciop-log "INFO" "Working with file $input"
 	file=`ciop-copy -o . $input`
 
+	[ $? != 0 ] && exit $ERR_NOINPUT
+
 	ciop-log "DEBUG" "ciop-copy output is $file"
 	
-	if [ ! -z "$file" ]
-	then
-		file=`basename $file`
+	file=`basename $file`
 
-		filetype=`echo $file | cut -c 1-10`
+	# checks if it's a RR, FR or FRS
+	filetype=`echo $file | cut -c 1-10`
 
-		#prepares the environment
-		ciop-log "DEBUG" "copying templates to megsdir"
-		mkdir -p ${megsDir}/configurations/Reference_Configuration
-		cd ${megsDir}/configurations/Reference_Configuration
-		cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/resources .
-		cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/configuration.conf .
-		mkdir -p job_groups/myjob
-		cd job_groups/myjob
-		cp -Rv /usr/local/MEGS_8.1/templates/* .
+	#prepares the environment
+	ciop-log "INFO" "copying templates to megsdir"
+	mkdir -p ${megsDir}/configurations/Reference_Configuration
+	cd ${megsDir}/configurations/Reference_Configuration
+	cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/resources .
+	cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/configuration.conf .
+	mkdir -p job_groups/myjob
+	cd job_groups/myjob
+	cp -Rv /usr/local/MEGS_8.1/templates/* .
 		
-		#sets the correct modifiers.db and value.txt
-		cp run/value.${filetype}.txt run/value.txt
-		cp run/modifiers.${filetype}.db run/modifiers.db
+	#sets the correct modifiers.db and value.txt
+	cp run/value.${filetype}.txt run/value.txt
+	cp run/modifiers.${filetype}.db run/modifiers.db
 	
-		sed -i "s#inputfile: #inputfile: $inputDir/$file#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/job.conf
-		sed -i "s#export DATABASE_DIR=.*#export DATABASE_DIR=$megsDir/configurations/Reference_Configuration/job_groups/myjob/run/database#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/run/run_megs.sh
+	# instantiates the templates
+	sed -i "s#inputfile: #inputfile: $inputDir/$file#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/job.conf
+	sed -i "s#export DATABASE_DIR=.*#export DATABASE_DIR=$megsDir/configurations/Reference_Configuration/job_groups/myjob/run/database#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/run/run_megs.sh
 
-		ciop-log "INFO" "Starting megs processor"
-		cd $megsDir/configurations/Reference_Configuration/job_groups/myjob/run
-		ln -s ${outputDir} output
-		sh run_megs.sh "$inputDir/$file" "$prdurls"
+	ciop-log "INFO" "Starting megs processor"
+	cd $megsDir/configurations/Reference_Configuration/job_groups/myjob/run
+	ln -s ${outputDir} output
+	sh run_megs.sh "$inputDir/$file" "$prdurls"
 
-		cd $outputDir
-		ciop-log "INFO" "Publishing output"
-		ciop-publish -m $outputDir/*.*
-		ciop-log "DEBUG" "ciop-publish exited with $?"
-	else
-		ciop-log "ERROR" "Error ciop-copy output is empty"
-	fi
+	[ $? != 0 ] && exit $ERR_MEGS
+
+	cd $outputDir
+	ciop-log "INFO" "Publishing output"
+	ciop-publish -m $outputDir/*.*
+	ciop-log "DEBUG" "ciop-publish exited with $?"
 
 	#clears the directory for the next file
 	rm -rf $megsDir/*
