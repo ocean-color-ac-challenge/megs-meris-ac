fdd41caed5f3c21e978fafdb2e1cf07de5dc2078
fbrito <fbrito@sb-10-16-10-20.dev.terradue.int>
2014-10-16 15:07:35 +0200

    update

--- src/main/app-resources/megs/run.sh
+++ src/main/app-resources/megs/run.sh
@@ -13,7 +13,9 @@ export LC_ALL="en_US.UTF-8"
 # define the exit codes
 SUCCESS=0
 ERR_NOINPUT=3
+ERR_NOADF=4
 ERR_MEGS=5
+
 # add a trap to exit gracefully
 function cleanExit ()
 {
@@ -23,6 +25,7 @@ function cleanExit ()
 		$SUCCESS) 	msg="Processing successfully concluded";;
 		$ERR_NOINPUT)	msg="Input not retrieved to local node";;
 		$ERR_MEGS)	msg="megs returned an error";;
+		$ERR_NOADF)	msg="Could not retrieve custom ADF";;
 		*)		msg="Unknown error";;
 	esac
 
@@ -42,7 +45,15 @@ inputDir=${megsDir}/input
 outputDir=${megsDir}/output
 
 prdurl="`ciop-getparam prdurl`"
-[ "$prdurl" == "default" ] && prdurl=""
+if [ "$prdurl" == "default" ]; then
+  prdurl=""
+else
+  ciop-log "INFO" "Getting custom ADFs from $prdurl"
+  localprd="`echo $prdurl | ciop-copy -U -o $TMPDIR -`"
+  [ $? != 0 ] && exit $ERR_NOADF
+  ciop-log "INFO" "Using custom ADFs with archive signature `md5sum $localprd`"
+  prdurl="file://$localprd"
+fi
 
 mkdir -p $megsDir
 
@@ -56,45 +67,42 @@ do
 	
 	ciop-log "INFO" "Working with file $input"
 	file=`ciop-copy -o . $input`
-
 	[ $? != 0 ] && exit $ERR_NOINPUT
-
-	ciop-log "DEBUG" "ciop-copy output is $file"
 	
-	file=`basename $file`
-
 	# checks if it's a RR, FR or FRS
-	filetype=`echo $file | cut -c 1-10`
+	filetype=`basename $file | cut -c 1-10`
 
 	#prepares the environment
-	ciop-log "INFO" "copying templates to megsdir"
+	ciop-log "INFO" "Preparing MEGS environment"
 	mkdir -p ${megsDir}/configurations/Reference_Configuration
 	cd ${megsDir}/configurations/Reference_Configuration
-	cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/resources .
-	cp -Rv /usr/local/MEGS_8.1/configurations/Reference_Configuration/configuration.conf .
+	cp -R /usr/local/MEGS_8.1/configurations/Reference_Configuration/resources .
+	cp -R /usr/local/MEGS_8.1/configurations/Reference_Configuration/configuration.conf .
 	mkdir -p job_groups/myjob
 	cd job_groups/myjob
-	cp -Rv /usr/local/MEGS_8.1/templates/* .
+	cp -R /usr/local/MEGS_8.1/templates/* .
 		
 	#sets the correct modifiers.db and value.txt
 	cp run/value.${filetype}.txt run/value.txt
 	cp run/modifiers.${filetype}.db run/modifiers.db
 	
 	# instantiates the templates
-	sed -i "s#inputfile: #inputfile: $inputDir/$file#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/job.conf
+	sed -i "s#inputfile: #inputfile: $file#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/job.conf
 	sed -i "s#export DATABASE_DIR=.*#export DATABASE_DIR=$megsDir/configurations/Reference_Configuration/job_groups/myjob/run/database#g" $megsDir/configurations/Reference_Configuration/job_groups/myjob/run/run_megs.sh
 
 	ciop-log "INFO" "Starting megs processor"
 	cd $megsDir/configurations/Reference_Configuration/job_groups/myjob/run
 	ln -s ${outputDir} output
-	sh run_megs.sh "$inputDir/$file" "$prdurl"
+
+	ciop-log "INFO" "Starting megs processor"
+	sh run_megs.sh "$inputDir/$file" $prdurl 
 
 	[ $? != 0 ] && exit $ERR_MEGS
 
-	cd $outputDir
+	#cd $outputDir
 	ciop-log "INFO" "Publishing output"
-	ciop-publish -m $outputDir/*.*
-	ciop-log "DEBUG" "ciop-publish exited with $?"
+	find $outputDir -type f -exec gzip \{\} \;
+	ciop-publish -m $outputDir/*
 
 	#clears the directory for the next file
 	rm -rf $megsDir/*
